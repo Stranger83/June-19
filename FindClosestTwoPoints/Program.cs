68ba043b0cd33cc47786082c527d784d3f03793e
Stranger <jmanova83@gmail.com>
2017-06-21 13:07:31 +0300

    05-06/07 done

---
+++ FindClosestTwoPoints/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindClosestTwoPoints
+{
+	class Point
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Point[] points = ReadPoints();
+			Point[] closestPoints = CalculateTwoClosestPoints(points);
+
+			PrintDistance(closestPoints);
+			PrintPoint(closestPoints[0]);
+			PrintPoint(closestPoints[1]);
+		}
+
+		private static void PrintDistance(Point[] closestPoints)
+		{
+			double distance = CalculateDistance(closestPoints[0], closestPoints[1]);
+			Console.WriteLine($"{distance:f3}");
+		}
+
+		private static double CalculateDistance(Point p1, Point p2)
+		{
+			int deltaX = p1.X - p2.X;
+			int deltaY = p1.Y - p2.Y;
+			double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+			return distance;
+		}
+
+		private static void PrintPoint(Point point)
+		{
+			Console.WriteLine($"({point.X}, {point.Y})");
+		}
+
+		private static Point[] CalculateTwoClosestPoints(Point[] points)
+		{
+			double minDistance = double.MaxValue;
+			Point[] closestPoints = null;
+			for (int p1 = 0; p1 < points.Length; p1++)
+			{
+				for (int p2 = p1 + 1; p2 < points.Length; p2++)
+				{
+					double distance = CalculateDistance(points[p1], points[p2]);
+					if (distance < minDistance)
+					{
+						minDistance = distance;
+						closestPoints = new Point[] { points[p1], points[p2] };
+					}
+				}
+
+			}
+			return closestPoints;
+		}
+
+		private static Point[] ReadPoints()
+		{
+			int n = int.Parse(Console.ReadLine());
+			Point[] points = new Point[n];
+			for (int i = 0; i < n; i++)
+			{
+				int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+				Point p = new Point();
+				p.X = input[0];
+				p.Y = input[1];
+				points[i] = p; 
+			}
+			return points;
+		}
+	}
+}
---
+++ RectanglePosition/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectanglePosition
+{
+	class Rectangle
+	{
+		public int Top { get; set; }
+		public int Left { get; set; }
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+		public int Bottom { get	{ return Top + Height;}	}
+		public int Right { get	{ return Left + Width;} }
+
+
+		public int CalcArea()
+		{
+			return Width * Height;
+		}
+		public bool IsInside(Rectangle other)
+		{
+			var isInLeft = Left >= other.Left;
+			var isInRight = Right <= other.Right;
+			var isInsideHorizontal = isInLeft && isInRight;
+			var isInTop = Top <= other.Top;
+			var isInBottom = Bottom <= other.Bottom;
+			var isInsideVertical = isInTop && isInBottom;
+			return isInsideHorizontal && isInsideVertical;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Rectangle r1 = ReadRectangle();
+			Rectangle r2 = ReadRectangle();
+			Console.WriteLine(r1.IsInside(r2) ? "Inside" : "Not inside");
+		}
+
+		private static Rectangle ReadRectangle()
+		{
+			int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			Rectangle r = new Rectangle
+			{
+				Top = input[1],
+				Left = input[0],
+				Width = input[2],
+				Height = input[3]
+			};
+			return r;
+		}
+	}
+}
